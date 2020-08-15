c2bb8e053db07f2c2da82343c207798b95cac8c6
douglasOlv <douglasolv.dev@gmail.com>
2020-08-15 17:30:53 -0300

    create user

--- src/app/controller/UserController.ts
+++ src/app/controller/UserController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express'
+import { getRepository, Repository } from 'typeorm'
+import User from '../model/User'
 
 class UsserController {
-    store(req: Request, res: Response) {
-        res.send("ok")
+  async store(req: Request, res: Response) {
+    const repository = getRepository(User)
+    const { email, password } = req.body;
 
+    const userExist = await repository.findOne({ where: { email } })
+
+    if (userExist) {
+      return res.sendStatus(409)
     }
+    const user = repository.create({ email, password })
+    await repository.save(user)
+    return res.status(201).json(user)
+  }
 
 }
 export default new UsserController();
